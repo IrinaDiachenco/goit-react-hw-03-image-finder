a6c8d0639ef7b3191c0578f10964b8d2172d307d
IrinaDiachenco <65997430+IrinaDiachenco@users.noreply.github.com>
2021-02-10 10:27:54 +0200

    update

--- src/API/PixabayFetch.jsx
+++ src/API/PixabayFetch.jsx
@@ -1,4 +1,4 @@
-export function fetchGallery(searchQuery, page = 1) {
+export default function fetchGallery(searchQuery, page) {
   return fetch(
     `https://pixabay.com/api/?q=${searchQuery}&page=${page}&key=19295709-d5276be665a4131ac0929716c&image_type=photo&orientation=horizontal&per_page=12`,
   ).then(response => {
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
 import { Component } from 'react';
 
-import {fetchGallery} from '../../API/PixabayFetch';
+import fetchGallery from '../../API/PixabayFetch';
 import s from './ImageGallery.module.css';
 import Preloader from '../Loader/Loader';
 import Modal from '../Modal/Modal';
@@ -9,7 +9,6 @@ import Button from "../Button/Button"
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
 
 import { toast } from 'react-toastify';
-//import { FALSE } from "node-sass";
 
 const Status = {
   IDLE: 'idle',
@@ -20,7 +19,7 @@ const Status = {
 
 export default class ImageGallery extends Component {
   static propTypes = {
-  searchQuery: PropTypes.string,
+    searchQuery: PropTypes.string,
   };
 
   state = {
@@ -37,10 +36,10 @@ export default class ImageGallery extends Component {
     status: Status.IDLE,
   };
 
-  async componentDidUpdate(prevProps, prevState, page) {
+  async componentDidUpdate(prevProps, prevState) {
     const prevSearch = prevProps.searchQuery;
     const nextSearch = this.props.searchQuery;
-    //const nextPage = this.state.page + 1;
+    //let nextPage = this.state.page + 1;
 
     if (prevSearch !== nextSearch) {
  
@@ -52,106 +51,109 @@ export default class ImageGallery extends Component {
 
       this.updatingGallery(nextSearch);
     }
-    if (prevSearch) {
+    if (this.state.gallery !== prevState.gallery) {
       this.scrollToBottom();
     }
   }
 
     scrollToBottom = () => {
-    //if (this.updatingGallery)
-    window.scrollTo({
+      //if (this.state.page !== 1)
+      window.scrollTo({
         top: document.documentElement.scrollHeight,
         behavior: "smooth",
       });
     }
 
-  handleLoadMore = () => {
-    this.updatingGallery(this.props.searchQuery);
-    this.setState({ status: Status.PENDING });
-  }
+    handleLoadMore = () => {
+      this.updatingGallery(this.props.searchQuery);
+      this.setState({ status: Status.PENDING });
+    }
 
-updatingGallery = (nextSearch) => {
-  const { page } = this.state;
+    updatingGallery = (nextSearch, prevState) => {
+      const { page } = this.state;
   
-  fetchGallery(nextSearch, page)
-    .then(response => {
-      if (response.hits.length === 0) {
-        this.setState({ status: Status.IDLE });
-        return toast.error('not found');
-      }
-      this.setState(
-        (prevState) => ({
-          gallery: [...prevState.gallery, ...response.hits],
-          status: Status.RESOLVED,
-          page: prevState.page + 1,
-        }));
-      //this.scrollToBottom();
-        //window.scrollTo(0,(page.scrollHeight))
-        //window.scrollByPages(1)
+      fetchGallery(nextSearch, page)
+        .then(response => {
+          if (response.hits.length === 0) {
+            this.setState({ status: Status.IDLE });
+            return toast.error('not found');
+          }
+          this.setState(
+            (prevState) => ({
+              gallery: [...prevState.gallery, ...response.hits],
+              status: Status.RESOLVED,
+              page: prevState.page + 1,
+            })
+            //this.scrollToBottom
+          );
+          //this.scrollToBottom();
+          //window.scrollTo(0,(page.scrollHeight))
+          //window.scrollByPages(1)
       
-    })
-    .catch(({ message }) =>
-      this.setState({ error: message, status: Status.REJECTED }),
-    )
-    //.finally(() => this.scrollToBottom());
-  //this.setState(prevState => ({ page: (prevState.page + 1) }));
-  }
-
-  imageClickHandler = (src, alt) => {
-    this.toggleModal();
-    this.setState({
-      modalImage: { src, alt },
-    });
-  };
-
-  handlerClickEsc = e => {
-    if (e.key === 'Escape' && this.state.modal.show) this.openModal();
-  };
-
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal,
-    }));
-  };
-
-  render() {
-    const { gallery, status, error, showModal, modalImage } = this.state;
-
-    if (status === 'idle') {
-      return <div></div>;
+        })
+        .catch(({ message }) =>
+          this.setState({ error: message, status: Status.REJECTED }),
+        )
+      //.finally(() => this.scrollToBottom());
+      //this.setState(prevState => ({ page: (prevState.page + 1) }));
+  
     }
 
-    if (status === 'pending') {
-      return <Preloader />;
+    imageClickHandler = (src, alt) => {
+      this.toggleModal();
+      this.setState({
+        modalImage: { src, alt },
+      });
+    };
+
+    handlerClickEsc = e => {
+      if (e.key === 'Escape' && this.state.modal.show) this.openModal();
+    };
+
+    toggleModal = () => {
+      this.setState(({ showModal }) => ({
+        showModal: !showModal,
+      }));
+    };
+
+    render() {
+      const { gallery, status, error, showModal, modalImage } = this.state;
+
+      if (status === 'idle') {
+        return <div></div>;
+      }
+
+      if (status === 'pending') {
+        return <Preloader />;
        
-    }
+      }
 
-    if (status === 'rejected') {
-      return <h1>{error}</h1>
-    }
+      if (status === 'rejected') {
+        return <h1>{error}</h1>
+      }
 
-    if (status === 'resolved') {
-      return (
-        <div>
-        <ul className={s.ImageGallery}>
-          {gallery.map(({ webformatURL, largeImageURL, tags }, index) => (
-            <ImageGalleryItem key={index}
-              webformatURL={webformatURL}
-              largeImageURL={largeImageURL}
-              tags={tags}
-              imageClickHandler={this.imageClickHandler}
-            />
-          ))}
-          </ul>
+      if (status === 'resolved') {
+        return (
+          <div>
+            <ul className={s.ImageGallery}>
+              {gallery.map(({ webformatURL, largeImageURL, tags }, index) => (
+                <ImageGalleryItem key={index}
+                  webformatURL={webformatURL}
+                  largeImageURL={largeImageURL}
+                  tags={tags}
+                  imageClickHandler={this.imageClickHandler}
+                />
+              ))}
+            </ul>
 
-          {showModal && (
-          <Modal image={modalImage} toggleModal={this.toggleModal} />
-          )}
+            {showModal && (
+              <Modal image={modalImage} toggleModal={this.toggleModal} />
+            )}
         
-          <Button onClick={this.handleLoadMore} />
-         </div> 
+            <Button onClick={this.handleLoadMore} />
+          </div>
           
-      );
+        );
+      }
     }
   }
-}
